8898998f1747870c0438f41da67bf06335ea99ee
max <Wht2826920647@outlook.com>
2026-01-28 12:31:57 +0800

    position_holder_2

--- src/PositionHolder.sol
+++ src/PositionHolder.sol
@@ -1,10 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-contract PositionHolder {
-    address public owner;
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
+contract PositionHolder {
+    address public immutable factory;
+    address public immutable market;
+
+    /*//////////////////////////////////////////////////////////////
+                                 SLOTS
+    //////////////////////////////////////////////////////////////*/
+    mapping(address user => mapping( address position => uint256 amount)) public mappingPositions;
+
+    /*//////////////////////////////////////////////////////////////
+                                  INIT
+    //////////////////////////////////////////////////////////////*/
     constructor() {
-        owner = msg.sender;
+        factory = msg.sender;
     }
+
+    function initialize() external {}
 }
