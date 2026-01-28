5762932a2945c444e24e7ddcbed6572878e129fc
max <Wht2826920647@outlook.com>
2026-01-28 11:14:52 +0800

    fix: src

---
+++ src/interface/ILiquidityChecker.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+/**
+ * @title ILiquidityChecker
+ * @author Max_2557
+ * @notice 这个合约用来检查polymaret中market的流动性是否符合抵押要求 (需要大于一个值)
+ */
+interface ILiquidityChecker {}
